7f7aca40584bfe34baa67ff17067a510b926e5ea
mahedihasssannir <mahedinir34678@gmail.com>
2024-11-04 01:20:34 +0600

    delition

--- linked list/linked-list.js
+++ linked list/linked-list.js
@@ -22,25 +22,46 @@ class linkedList {
         this.size++
     }
 
-    traversing = () => {
-        let counter = 0;
-        let currentNode = this.head;
-        while (counter < this.size) {
-            console.log(currentNode);
-            currentNode = currentNode.next;
-            counter++
-        };
+    // traversing = () => {
+    //     let counter = 0;
+    //     let currentNode = this.head;
+    //     while (counter < this.size) {
+    //         console.log(currentNode);
+    //         currentNode = currentNode.next;
+    //         counter++
+    //     };
+    // };
+    deleteNode = (index) => {
+
+        let counter = 1;
+        let lead = this.head;
+        if (index === 1) {
+            this.head = this.head.next;
+        } else {
+            while (counter < index - 1) {
+                lead = lead.next;
+                counter++
+            }
+            let nextNode = lead.next.next
+            lead.next = nextNode;
+            console.log(lead);
+        }
+
+
     };
 };
 
 
 let newLinedList = new linkedList(100);
 newLinedList.appendNode(200);
-newLinedList.appendNode(200);
-newLinedList.appendNode(30);
+newLinedList.appendNode(300);
 newLinedList.appendNode(400);
 newLinedList.appendNode(500);
 newLinedList.appendNode(600);
 newLinedList.appendNode(700);
-newLinedList.traversing()
+newLinedList.appendNode(800);
+// newLinedList.traversing()
+newLinedList.deleteNode(2)
+newLinedList.deleteNode(5)
+
 console.log(newLinedList);
